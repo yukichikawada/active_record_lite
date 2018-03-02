77fde9eada9812784caf2e13974c70e8cc7be019
Yukichi Kawada <yukichikawada@gmail.com>
2018-03-02 14:18:39 -0800

    Complete Phase 1a

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -12,11 +12,11 @@ class SQLObject
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    @table_name ||= 'cats'
   end
 
   def self.all
