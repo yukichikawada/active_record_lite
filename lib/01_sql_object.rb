410bf057959d5d4463e1202b462c6ac9dc03db20
Yukichi Kawada <yukichikawada@gmail.com>
2018-03-02 16:50:50 -0800

    Complete phase 1b

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -5,10 +5,27 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    return @columns if @columns
+    cols = DBConnection.execute2(<<-SQL).first
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      SQL
+    cols.map!(&:to_sym)
+    @columns = cols
   end
 
   def self.finalize!
+    self.columns.each do |name|
+      define_method(name) do
+        self.attributes[name]
+      end
+
+      define_method("#{name}=") do |val|
+        self.attributes[name] = val
+      end
+    end
   end
 
   def self.table_name=(table_name)
@@ -20,38 +37,80 @@ class SQLObject
   end
 
   def self.all
-    # ...
+    data = DBConnection.execute(<<-SQL)
+      SELECT
+        #{table_name}.*
+      FROM
+        #{table_name}
+    SQL
+
+    parse_all(data)
   end
 
   def self.parse_all(results)
-    # ...
+    results.map { |hash| self.new(hash) }
   end
 
   def self.find(id)
-    # ...
+    res = DBConnection.execute(<<-SQL, id)
+      SELECT
+        #{table_name}.*
+      FROM
+        #{table_name}
+      WHERE
+        #{table_name}.id = ?
+    SQL
+
+    parse_all(res).first
   end
 
   def initialize(params = {})
-    # ...
+    params.each do |attr_name, value|
+      col = attr_name.to_sym
+      if self.class.columns.include? col
+        self.send("#{col}=", value)
+      else
+        raise "unknown attribute '#{attr_name}'"
+      end
+    end
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
-    # ...
+    self.class.columns.map { |col| self.send(col) }
   end
 
   def insert
-    # ...
+    # col_names = columns into array
+    columns = self.class.columns.drop(1)
+    column_names = columns.map(&:to_s).join(", ")
+    # question_marks = array of ? rep length insertion
+    q_places = (["?"] * columns.length).join(", ")
+    DBConnection.execute(<<-SQL, *attribute_values.drop(1))
+      INSERT INTO
+        #{self.class.table_name} (#{column_names})
+      VALUES
+        (#{q_places})
+    SQL
+    self.id = DBConnection.last_insert_row_id
   end
 
   def update
-    # ...
+    resets = self.class.columns.map { |col| "#{col} = ?" }.join(", ")
+    DBConnection.execute(<<-SQL, *attribute_values, id)
+      UPDATE
+        #{self.class.table_name}
+      SET
+        #{resets}
+      WHERE
+        #{self.class.table_name}.id = ?
+    SQL
   end
 
   def save
-    # ...
+    id.nil? ? insert : update
   end
 end
